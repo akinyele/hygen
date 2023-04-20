b94bfcbdcb6af90152e51507ee7fcccabcd4cf04
Akinyele <akinyele@bitovi.com>
2023-04-20 10:55:48 -0500

    - cleanup, prettier changes.

--- src/rag/const.ts
+++ src/rag/const.ts
@@ -1,7 +1,3 @@
-// "@storybook/react": "^7.0.2",
-//   "@testing-library/react": "^14.0.0",
-//   "@types/jest": "^29.5.0",
-
-export const STORY_BOOK = "@storybook/react"
-export const JEST = "@types/jest"
-export const TESTING_LIBRARY = "@testing-library/react"
+export const STORY_BOOK = '@storybook/react'
+export const JEST = '@types/jest'
+export const TESTING_LIBRARY = '@testing-library/react'
